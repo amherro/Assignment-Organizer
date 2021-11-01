a8c8145e51441cb34cf163b1d30bef6255ac938b
Adam Herro <adam.herro@comcast.net>
2021-11-01 16:05:34 -0400

    Adding dynamic styling to assignments when marking them complete.

--- src/Components/Assignment.js
+++ src/Components/Assignment.js
@@ -1,28 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/Assignment.css'
 import { FaTimes, FaCheckCircle } from 'react-icons/fa'
 
 const Assignment = ({ name, subject }) => {
+    const [complete, setComplete] = useState(false);
+
+    const handleSetComplete = (e) => {
+        e.preventDefault();
+        setComplete(!complete)
+        if(complete === true) {
+            document.querySelector('.complete-btn').style.color = 'green'
+            document.querySelector('.assignment-card').style.opacity = '0.45'
+        } else {
+            document.querySelector('.complete-btn').style.color = 'rgb(172, 172, 172)'
+            document.querySelector('.assignment-card').style.opacity = '1.0'
+        }
+    }
     return (
-        <div className='assignment-card'>
-            <div className='top-of-card'>
-                <FaCheckCircle 
-                        style={{color: 'green', cursor: 'pointer'}}
-                        className='complete-btn'
-                />
-                <h6 className='assignment-names'>
-                    {name}
-                </h6>
-                <FaTimes 
-                    style={{color: 'red', cursor: 'pointer'}} 
-                    className='delete-btn'
-                />
+        <div className='assignment-section'>
+            <FaCheckCircle 
+                onClick={handleSetComplete}
+                
+                className='complete-btn'
+            />
+            <div className='assignment-card'>
+                <div className='top-of-card'>
+                    <p className='assignment-names'>
+                        {name}
+                    </p>
+                    <FaTimes 
+                        style={{color: 'red', cursor: 'pointer'}} 
+                        className='delete-btn'
+                    />
+                </div>
+                <p className='assignment-date bottom-of-card'>
+                    {`Class: ${subject}`}
+                </p>
             </div>
-            <p className='assignment-date bottom-of-card'>
-                {subject}
-            </p>
         </div>
     )
 }
+
+// const completeStyle = {
+//     style: {
+
+//     }
+// }
 
 export default Assignment
