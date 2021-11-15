81bb28f38e08936238d6ad9ec45113ac6198218a
Adam Herro <adam.herro@comcast.net>
2021-11-15 16:10:33 -0500

    Changing size of assignment card and implementing function to add date to local storage

--- src/Components/AssignmentCalendar.js
+++ src/Components/AssignmentCalendar.js
@@ -7,18 +7,52 @@ import '../Styles/AssignmentCalendar.css'
 const AssignmentCalendar = () => {
     const [dates, setDates] = useState([]);
     const [showDateInput, setShowDateInput] = useState(false);
+    const [hasItems, setHasItems] = useState(false);
     
+    // const addDateOnLoad = () => {
+    //     document.addEventListener('DOMContentLoaded', () => {
+    //         for (let i = 0; i < dates.length; i++) {
+    //             setDates(dates.concat({
+    //                 id: localStorage.key(i),
+    //                 date: localStorage.getItem(i)
+    //             }))
+    //         }
+    //         return (
+    //             <div className='date-display'>
+    //                 {dates.map(({ date, id }) => {
+    //                     return (
+    //                         <div key={id}>
+    //                             <Date date={date} id={id} deleteDate={deleteDate} addDateToLocalStorage={addDateToLocalStorage(id, date)}  />
+    //                         </div>
+    //                     )
+    //                 })}
+    //             </div>
+    //         )
+    //     })
+    // }
     const addDate = (date) => {
         setDates(dates.concat({
             id: uuidv4(),
             date,
         }))
+        setHasItems(hasItems === true)
     }
+    const addDateToLocalStorage = (id, date) => {
+        localStorage.setItem(`${id}`, date);
+    }
+    // const getDatesFromLocalStorage = (id) => {
+    //     let storedDates = JSON.parse(localStorage.getItem(id));
+    // }
     const toggleDateInput = () => {
         setShowDateInput(!showDateInput)
     }
     const deleteDate = (id) => {
         setDates(dates.filter((date) => date.id !== id))
+    }
+    const checkIfHasItems = () => {
+        if (dates.length === 0) {
+            setHasItems(hasItems === false);
+        }
     }
 
     return (
@@ -32,7 +66,7 @@ const AssignmentCalendar = () => {
                     {showDateInput && <DatesInput className='display' dates={dates} addDate={addDate}/>}
                 </div>
                 <div className='date-display-area'>
-                    <DatesDisplay dates={dates} deleteDate={deleteDate} />
+                    <DatesDisplay dates={dates} deleteDate={deleteDate} addDateToLocalStorage={addDateToLocalStorage} checkIfHasItems={checkIfHasItems} />
                 </div>
             </div>
         </main>
--- src/Components/DatesDisplay.js
+++ src/Components/DatesDisplay.js
@@ -2,13 +2,20 @@ import React from 'react'
 import Date from './Date';
 import '../Styles/DatesDisplay.css'
 
-const DatesDisplay = ({ dates, deleteDate }) => {
+const DatesDisplay = ({ dates, deleteDate, addDateToLocalStorage }) => {
+
+    // const getDatesFromLocalStorage = () => {
+    //     dates.map((id) => {
+    //         return localStorage.getItem(id);
+    //     })
+    // }
+
     return (
         <div className='date-display'>
             {dates.map(({ date, id }) => {
                 return (
                     <div key={id}>
-                        <Date date={date} id={id} deleteDate={deleteDate} />
+                        <Date date={date} id={id} deleteDate={deleteDate} addDateToLocalStorage={addDateToLocalStorage(id, date)}  />
                     </div>
                 )
             })}
