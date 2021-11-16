9b5fc6a078a2f1a5d0a13523998fd6508e53c59c
Adam Herro <adam.herro@comcast.net>
2021-11-15 19:14:24 -0500

    Working on functionality to get items from local storage and display on screen after window loads. Still working on this functionality

--- src/Components/AssignmentCalendar.js
+++ src/Components/AssignmentCalendar.js
@@ -7,52 +7,34 @@ import '../Styles/AssignmentCalendar.css'
 const AssignmentCalendar = () => {
     const [dates, setDates] = useState([]);
     const [showDateInput, setShowDateInput] = useState(false);
-    const [hasItems, setHasItems] = useState(false);
     
-    // const addDateOnLoad = () => {
-    //     document.addEventListener('DOMContentLoaded', () => {
-    //         for (let i = 0; i < dates.length; i++) {
-    //             setDates(dates.concat({
-    //                 id: localStorage.key(i),
-    //                 date: localStorage.getItem(i)
-    //             }))
-    //         }
-    //         return (
-    //             <div className='date-display'>
-    //                 {dates.map(({ date, id }) => {
-    //                     return (
-    //                         <div key={id}>
-    //                             <Date date={date} id={id} deleteDate={deleteDate} addDateToLocalStorage={addDateToLocalStorage(id, date)}  />
-    //                         </div>
-    //                     )
-    //                 })}
-    //             </div>
-    //         )
-    //     })
-    // }
+    const addDateOnLoad = () => {
+        document.addEventListener('DOMContentLoaded', () => {
+            for (let i = 0; i < localStorage.length; i++) {
+                console.log(i)
+                setDates(dates.concat({
+                    id: localStorage.key(i),
+                    date: JSON.parse(localStorage.getItem(`${i}`)),
+                    
+                }))
+            }
+    
+        })
+    }
     const addDate = (date) => {
         setDates(dates.concat({
             id: uuidv4(),
             date,
         }))
-        setHasItems(hasItems === true)
     }
     const addDateToLocalStorage = (id, date) => {
-        localStorage.setItem(`${id}`, date);
+        (localStorage.setItem(`${id}`, JSON.stringify(date)));
     }
-    // const getDatesFromLocalStorage = (id) => {
-    //     let storedDates = JSON.parse(localStorage.getItem(id));
-    // }
     const toggleDateInput = () => {
         setShowDateInput(!showDateInput)
     }
     const deleteDate = (id) => {
         setDates(dates.filter((date) => date.id !== id))
-    }
-    const checkIfHasItems = () => {
-        if (dates.length === 0) {
-            setHasItems(hasItems === false);
-        }
     }
 
     return (
@@ -66,7 +48,7 @@ const AssignmentCalendar = () => {
                     {showDateInput && <DatesInput className='display' dates={dates} addDate={addDate}/>}
                 </div>
                 <div className='date-display-area'>
-                    <DatesDisplay dates={dates} deleteDate={deleteDate} addDateToLocalStorage={addDateToLocalStorage} checkIfHasItems={checkIfHasItems} />
+                    <DatesDisplay dates={dates} deleteDate={deleteDate} addDateToLocalStorage={addDateToLocalStorage} addDateOnLoad={addDateOnLoad()} />
                 </div>
             </div>
         </main>
--- src/Components/DatesDisplay.js
+++ src/Components/DatesDisplay.js
@@ -4,18 +4,12 @@ import '../Styles/DatesDisplay.css'
 
 const DatesDisplay = ({ dates, deleteDate, addDateToLocalStorage }) => {
 
-    // const getDatesFromLocalStorage = () => {
-    //     dates.map((id) => {
-    //         return localStorage.getItem(id);
-    //     })
-    // }
-
     return (
         <div className='date-display'>
             {dates.map(({ date, id }) => {
                 return (
                     <div key={id}>
-                        <Date date={date} id={id} deleteDate={deleteDate} addDateToLocalStorage={addDateToLocalStorage(id, date)}  />
+                        <Date date={date} id={id} deleteDate={deleteDate} addDateToLocalStorage={addDateToLocalStorage(id, date)} />
                     </div>
                 )
             })}
